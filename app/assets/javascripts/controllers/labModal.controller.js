395b11f940913bf96ec49d5401561d29b1637065
Andrey Tatarenko <andrey17076@gmail.com>
2017-05-14 12:31:46 +0300

    add state changes

--- app/assets/javascripts/controllers/labModal.controller.js
+++ app/assets/javascripts/controllers/labModal.controller.js
@@ -5,6 +5,13 @@ angular.module('app').controller('LabModalController', function($mdDialog, Lab, 
     self.subjectId = subjectId;
     self.lab = lab;
     self.updateMode = !!self.lab;
+    self.states = ['todo', 'in_progress', 'resolved', 'done'];
+    self.stateMapping = {
+        todo: 'Todo',
+        in_progress: 'In Progress',
+        resolved: 'Resolved',
+        done: 'Done'
+    };
 
     if (self.updateMode) {
         self.title = 'Lab';
