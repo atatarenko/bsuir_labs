5af3ab1a221f9c13973e8c0a4bf7284af977fd29
Andrey Tatarenko <atatarenko@github.com>
2017-12-10 15:18:46 +0300

    Refactor subject controller

--- app/controllers/api/subjects_controller.rb
+++ app/controllers/api/subjects_controller.rb
@@ -1,22 +1,24 @@
 # frozen_string_literal
 module Api
   class SubjectsController < BaseController
-
     def index
       render json: subjects, status: :ok
     end
 
     def show
-      subject = subjects.find(params[:id])
-      if subject.present?
-        render json: subject, status: :ok
-      else
-        render nothing: true, status: :not_found
-      end
+      render json: subject, status: :ok
     end
 
     def create
-      subject = subjects.new(prepare_params)
+      if new_subject.save
+        render json: new_subject, status: :ok
+      else
+        render json: { errors: new_subject.errors.messages }, status: :bad_request
+      end
+    end
+
+    def update
+      subject.assign_attributes(prepare_params)
       if subject.save
         render json: subject, status: :ok
       else
@@ -24,31 +26,20 @@ module Api
       end
     end
 
-    def update
-      subject = subjects.find(params[:id])
-      if subject.present?
-        subject.assign_attributes(prepare_params)
-        if subject.save
-          render json: subject, status: :ok
-        else
-          render json: { errors: subject.errors.messages }, status: :bad_request
-        end
-      else
-        render nothing: true, status: :not_found
-      end
-    end
-
     def destroy
-      subject = subjects.find(params[:id])
-      if subject.present?
-        subject.destroy
-        render nothing: true, status: :ok
-      else
-        render nothing: true, status: :not_found
-      end
+      subject.destroy
+      head :ok
     end
 
     private
+
+    def subject
+      @subject ||= subjects.find(params[:id])
+    end
+
+    def new_subject
+      @new_subject ||= subjects.new(prepare_params)
+    end
 
     def prepare_params
       params
